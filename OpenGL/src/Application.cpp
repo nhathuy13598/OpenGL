8dd7b1e414392858a33a8722c26f5003071a9899
nhathuy <nhathuy13598@gmail.com>
2020-12-19 10:10:29 +0700

    - Draw first triangle

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -25,6 +25,12 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glBegin(GL_TRIANGLES);
+        glVertex2d(-0.5, -0.5);
+        glVertex2d(0, 0.5);
+        glVertex2d(0.5, -0.5);
+        glEnd();
+
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
