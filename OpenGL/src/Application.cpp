509e2fee200549554d0321b90621145177910075
NhatHuyTran <nhathuy13598@gmail.com>
2021-01-01 14:56:28 +0700

    - Init Glew

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -1,5 +1,6 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
-
+#include <iostream>
 int main(void)
 {
     GLFWwindow* window;
@@ -15,9 +16,18 @@ int main(void)
         glfwTerminate();
         return -1;
     }
-
+    
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK)
+    {
+        std::cout << "Error!" << std::endl;
+    }
+
+    std::cout << glGetString(GL_VERSION) << std::endl;
+    size_t a;
+    glGenBuffers(&a);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
